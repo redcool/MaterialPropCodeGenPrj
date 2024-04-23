677b1bf820ac273c89cfc9b5c60944c4b47d1fdb
gamebox1 <redcool007@qq.com>
2024-04-23 16:34:47 +0800

    v(0.0.1.1) add Tools

---
+++ MaterialPropCodeGenPrj/MaterialPropCodeGen/Tools/EditorGroupAttribute.cs
@@ -0,0 +1,121 @@
+﻿namespace PowerUtilities
+{
+    using System;
+    using UnityEngine;
+#if UNITY_EDITOR
+    using UnityEditor;
+
+    [CustomPropertyDrawer(typeof(EditorGroupAttribute))]
+    public class EditorGroupDrawer : PropertyDrawer
+    {
+        (string groupName, bool isOn) groupInfo;
+
+        const float LINE_HEIGHT = 20;
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var groupAttr = attribute as EditorGroupAttribute;
+            var isGroupOn = MaterialGroupTools.IsGroupOn(groupAttr.groupName);
+            // header path
+            if (groupAttr.isHeader)
+            {
+                if(!isGroupOn)
+                    return LINE_HEIGHT;
+
+                // prop height
+                var height = EditorGUI.GetPropertyHeight(property, property.isExpanded);
+                // add header's height
+                    height += EditorGUIUtility.singleLineHeight;
+                return height;
+            }
+
+            // other content path
+            if (isGroupOn)
+            {
+                return EditorGUI.GetPropertyHeight(property, property.isExpanded);
+            }
+
+            return -2;
+        }
+
+        public static bool DrawTitleFoldout(Rect position, bool isOn, string title, Color titleColor)
+        {
+            var pos = position;
+            pos.height = LINE_HEIGHT;
+            var colorRect = EditorGUI.IndentedRect(pos);
+            //EditorGUI.DrawRect(colorRect, titleColor);
+
+            // show only a style
+            GUI.BeginGroup(pos,(GUIStyle)"Button");
+            GUI.EndGroup();
+
+            var isFold = EditorGUI.Foldout(pos, isOn, title, true);
+            return isFold;
+        }
+
+        public static bool DrawTitleFoldout1(Rect position, bool isOn, string title, Color titleColor)
+        {
+            var colorRect = EditorGUI.IndentedRect(position);
+            EditorGUI.DrawRect(colorRect, titleColor);
+
+            return EditorGUI.Foldout(position, isOn, title, true);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var groupAttr = attribute as EditorGroupAttribute;
+
+            //return;
+            groupInfo.groupName = groupAttr.groupName;
+            groupInfo.isOn = MaterialGroupTools.IsGroupOn(groupAttr.groupName);
+
+            //draw header
+            if (groupAttr.isHeader)
+            {
+                GUIStyle style = "Box";
+                //style.padding.left=15;
+                style.alignment = TextAnchor.LowerLeft;
+
+                if (groupInfo.isOn)
+                    position.height -= LINE_HEIGHT;
+
+                groupInfo.isOn = DrawTitleFoldout(position, groupInfo.isOn, groupInfo.groupName, groupAttr.titleColor);
+
+                MaterialGroupTools.SetState(groupInfo.groupName, groupInfo.isOn);
+                position.y += LINE_HEIGHT;
+            }
+
+            //draw contents
+            if (!groupInfo.isOn)
+                return;
+
+            EditorGUI.indentLevel++;
+            EditorGUI.PropertyField(position, property, new GUIContent(property.displayName), true);
+            EditorGUI.indentLevel--;
+        }
+    }
+#endif
+
+    /// <summary>
+    /// Show toogle group in inspector gui( fields )
+    /// 
+    /// [EditorGroup("Fog",true)] public bool _IsGlobalFogOn;
+    /// [EditorGroup("Fog")][Range(0, 1)] public float _GlobalFogIntensity = 1;
+    /// </summary>
+    /// 
+    [AttributeUsage(AttributeTargets.Field)]
+    public class EditorGroupAttribute : PropertyAttribute
+    {
+        public string groupName;
+        public bool isHeader;
+        public Color titleColor;
+
+        public EditorGroupAttribute(string groupName, bool isHeader = false, string titleColorStr = "#404E7C")
+        {
+            this.isHeader = isHeader;
+            this.groupName=groupName;
+            ColorUtility.TryParseHtmlString(titleColorStr, out titleColor);
+        }
+    }
+
+}
---
+++ MaterialPropCodeGenPrj/MaterialPropCodeGen/Tools/MaterialGroupTools.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PowerUtilities
+{
+    public static class MaterialGroupTools
+    {
+        public class GroupInfo
+        {
+            public bool isOn; // this group unfold?
+            public bool hasCheckedMark; // isChecked work when hasCheckedMark is true
+            public bool isChecked; //this group enabled?
+            public Color backgrounColor = new Color(0.05f, 0.05f, 0.05f, 0.05f); 
+            public Color columnColor = Color.blue*0.5f; // show group column header color
+        }
+
+        public const string DEFAULT_GROUP_NAME = "_";
+        public const float BASE_LABLE_WIDTH = 162.5f;
+
+        public readonly static Dictionary<string, GroupInfo> groupInfoDict = new Dictionary<string,GroupInfo>();
+
+        public static bool IsDefaultGroup(string groupName) => string.IsNullOrEmpty(groupName) ||
+            groupName == DEFAULT_GROUP_NAME ||
+            !groupInfoDict.ContainsKey(groupName);
+
+        public static int GroupIndentLevel(string groupName) => IsDefaultGroup(groupName) ? 0 : 1;
+        public static Color GetBackgroundColor(string groupName) => GetGroupInfo(groupName).backgrounColor;
+        public static Color GetColumnColor(string groupName) => GetGroupInfo(groupName).columnColor;
+
+        public static bool IsGroupOn(string groupName)
+        {
+            // default Group or not Group open always
+            if (IsDefaultGroup(groupName))
+                return true;
+
+            var info = GetGroupInfo(groupName);
+            return info.isOn;
+        }
+
+        public static bool IsGroupDisabled(string groupName)
+        {
+            var info = GetGroupInfo(groupName);
+            return info.hasCheckedMark ? !info.isChecked : false;
+        }
+
+        public static void SetState(string groupName, bool isOn,bool hasCheckedMark = false,bool isChecked=false)
+        {
+            var info = GetGroupInfo(groupName);
+            info.isChecked = isChecked;
+            info.hasCheckedMark = hasCheckedMark;
+            info.isOn= isOn;
+        }
+
+        public static void SetStateAll(bool isOn,bool hasCheckedMark=false,bool isChecked=false)
+        {
+            var keys = groupInfoDict.Keys.ToArray();
+            foreach (var groupName in keys)
+            {
+                SetState(groupName,isOn, hasCheckedMark, isChecked);
+            }
+        }
+
+        public static GroupInfo GetGroupInfo(string groupName)
+        {
+            if (!groupInfoDict.TryGetValue(groupName, out var groupInfo))
+            {
+                groupInfo = groupInfoDict[groupName] = new GroupInfo();
+            }
+            return groupInfo;
+        }
+    }
+}
